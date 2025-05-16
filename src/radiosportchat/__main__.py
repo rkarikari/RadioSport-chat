cba7515594b7a51b371601ff08720407653f3c74
rkarikari <rk.9g5ar@gmail.com>
2025-05-16 11:25:50 +0000

    v2.5.4

--- src/radiosportchat/__main__.py
+++ src/radiosportchat/__main__.py
@@ -3,7 +3,7 @@ import os
 
 # Get the path to main.py
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-MAIN_SCRIPT = os.path.join(BASE_DIR, "main.py")
+MAIN_SCRIPT = os.path.join(BASE_DIR, "rag.py")
 
 # Run the Streamlit app
 if __name__ == "__main__":
--- src/radiosportchat/bbcrypt.py
+++
@@ -1,4 +0,0 @@
-import bcrypt
-password = "@dm1n!@#"
-hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-print(hashed.decode('utf-8'))  # Output: e.g., $2b$12$...
